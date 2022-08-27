3101746c4881b957cd1c8578d69cef30fe562609
aryan-debug <arynarora232004@gmail.com>
2022-08-27 16:41:45 -0700

    stored mongodb uri in env

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const io = new Server(server, { cors: "http://localhost:3000" });
 const mongoose = require('mongoose');
 const Room = require('./models/room');
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
