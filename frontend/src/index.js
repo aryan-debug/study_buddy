17e7677e386fd914d0cb518c4e786b4cef20aa52
aryan-debug <arynarora232004@gmail.com>
2022-08-27 19:22:52 -0700

    trying to change color mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/Homepage';
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeProvider, ColorModeScript } from '@chakra-ui/react'
 import theme from './theme';
 import {
   BrowserRouter,
@@ -15,15 +15,17 @@ import Room from './components/Room';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider theme={theme}>
-    <ColorModeProvider value='dark'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<App />} />
-          <Route path='/create_room' element={<CreateRoomPage />} />
-          <Route path="/rooms/:id" element={<Room />} />
-          <Route path="*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </ColorModeProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode}>
+      <ColorModeProvider value='dark'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='/create_room' element={<CreateRoomPage />} />
+            <Route path="/rooms/:id" element={<Room />} />
+            <Route path="*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </ColorModeProvider>
+    </ColorModeScript>
   </ChakraProvider>
 );
