4e160d3a06bf9f9b62978f1e023399e94509ea00
aryan-debug <arynarora232004@gmail.com>
2022-08-27 20:02:42 -0700

    updated image

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/Homepage';
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import theme from './theme';
 import {
   BrowserRouter,
@@ -17,15 +17,14 @@ localStorage.setItem("chakra-ui-color-mode", "dark")
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider theme={theme} colorModeManager={localStorage}>
-    <ColorModeProvider value='dark'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<App />} />
-          <Route path='/create_room' element={<CreateRoomPage />} />
-          <Route path="/rooms/:id" element={<Room />} />
-          <Route path="*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </ColorModeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/create_room' element={<CreateRoomPage />} />
+        <Route path="/rooms/:id" element={<Room />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+
   </ChakraProvider>
 );
