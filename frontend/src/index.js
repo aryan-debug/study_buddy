df2e7efdd24ddd527637992550af3f7b3613321d
aryan-debug <arynarora232004@gmail.com>
2022-08-27 19:30:21 -0700

    trying to change color mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/Homepage';
-import { ChakraProvider, ColorModeProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import theme from './theme';
 import {
   BrowserRouter,
